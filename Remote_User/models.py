5f26706930ebfd9f2e2e5726798ae14083934a5e
lokesh <lokeshedalapati@gmailo.com>
2025-08-07 13:30:14 +0530

    Initial commit of project files

---
+++ Remote_User/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+from django.db.models import CASCADE
+
+class ClientRegister_Model(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+    password = models.CharField(max_length=10)
+    phoneno = models.CharField(max_length=10)
+    country = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    gender= models.CharField(max_length=30)
+    address= models.CharField(max_length=30)
+
+class predict_chronic_diseases(models.Model):
+
+    Fid= models.CharField(max_length=300)
+    Gender= models.CharField(max_length=300)
+    Age= models.CharField(max_length=300)
+    ADDRESS= models.CharField(max_length=300)
+    CITY= models.CharField(max_length=300)
+    Smoking= models.CharField(max_length=300)
+    Diabetic= models.CharField(max_length=300)
+    KidneyDisease= models.CharField(max_length=300)
+    marital_status= models.CharField(max_length=300)
+    highest_qualification= models.CharField(max_length=300)
+    children= models.CharField(max_length=300)
+    Prediction= models.CharField(max_length=300)
+
+class detection_accuracy(models.Model):
+
+    names = models.CharField(max_length=300)
+    ratio = models.CharField(max_length=300)
+
+class detection_ratio(models.Model):
+
+    names = models.CharField(max_length=300)
+    ratio = models.CharField(max_length=300)
+
+
+
